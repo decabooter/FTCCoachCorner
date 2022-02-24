320895b9865fcba5a4c862a2544f6145ba256880
decabooter <decabooter@gmail.com>
2022-02-23 19:58:45 -0600

    initial plotly implementation--haven't figured out how to call it from index.js, but it's working with a direct call in my handlebars <head>

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require ('express');
 const path = require('path');
 //const exphbs = require('express-handlebars');
 const {engine} = require('express-handlebars');
+const plotly = require('plotly');
+//const plotly = require('plotly')("decabooter","c3EHAMO76RXLvFhezzyp");
+//const plotly = require('plotly.js-dist');
 const logger = require('./middleware/logger');
 const app = express();
 
@@ -12,14 +15,15 @@ const app = express();
 //app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.engine('handlebars', engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use(express.static("public/images"))
+app.use(express.static("public/images"));
+//app.use(plotly());
 
 //Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => res.render('index', {
-    title: 'Odometry',
+    title: 'Odometry'
 }));
 
 const { application } = require('express');
@@ -33,4 +37,4 @@ app.use('/api/odometry', require ('./routes/api/odometry'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('Server started on port ${PORT}'));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
